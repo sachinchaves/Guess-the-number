ef87a7de196de36f6b0308b74316de0763fa6c9d
sachinchaves <sachin.chaves1991@gmail.com>
2024-11-22 22:52:58 -0800

    refactored code

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 //When we click on Again, we reset the game but the highscore remains unchanged.
 
-let randomNumber = Math.floor(Math.random() * 21);
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
 console.log("randomNumber", randomNumber);
 let button = document.querySelector(".check");
 let score = 20;
@@ -18,35 +18,36 @@ button.addEventListener("click", function () {
   let guessedNumber = document.querySelector(".guess").value;
 
   // check if the number entered is empty, if so return false
-  if (guessedNumber === "" || guessedNumber === null) {
+  if (!guessedNumber) {
     message.textContent = "No Number!";
   } else if (Number(guessedNumber) === randomNumber) {
     // if the guessed number matches
     document.querySelector(".number").textContent = guessedNumber;
     message.textContent = "Correct Number!";
-    document.querySelector(".highscore").textContent = score;
-    document.querySelector("body").style.backgroundColor = "#60b347";
-  } else if (Number(guessedNumber) < randomNumber) {
-    // if the guessed number is less
-    if (score > 0) {
-      message.textContent = "Too Low!";
-      score -= 1;
-      document.querySelector(".score").textContent = score;
+    if (score > Number(document.querySelector(".highscore").textContent)) {
+      document.querySelector(".highscore").textContent = score;
     }
-  } else if (Number(guessedNumber) > randomNumber) {
-    // if the guessed number is high
-    if (score > 0) {
-      message.textContent = "Too High!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+  } else if (Number(guessedNumber) !== randomNumber) {
+    // if the guessed number is less
+    if (score > 1) {
+      message.textContent =
+        guessedNumber < randomNumber ? "Too Low!" : "Too High!";
       score -= 1;
       document.querySelector(".score").textContent = score;
+    } else {
+      message.textContent = "You Lost";
+      document.querySelector(".score").textContent = 0;
     }
   }
 });
 
 // Again Click
 document.querySelector(".again").addEventListener("click", function () {
-  let randomNumber = Math.floor(Math.random() * 21);
+  randomNumber = Math.floor(Math.random() * 20) + 1;
+  console.log("randomNumber1", randomNumber);
   score = 20;
+  document.querySelector(".score").textContent = 20;
   document.querySelector("body").style.backgroundColor = "#222";
   message.textContent = "Start guessing...";
   document.querySelector(".number").textContent = "?";
